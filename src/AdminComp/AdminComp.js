785dc81c30e402f1340fec96c788b38e776d714b
ANJANI MISHRA <38608850+aniraj2206@users.noreply.github.com>
2019-12-11 15:00:11 +0530

    data updated
    data

--- src/AdminComp/AdminComp.js
+++ src/AdminComp/AdminComp.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Data from "./../Modeldata/Data";
 
 class AdminComp extends Component {
-  
+  showData() {
+    console.log(Data);
+  }
   render() {
     return (
       <div>
-        <p> Admin services </p>
+        <p onClick={this.showData}> Admin services </p>
       </div>
     );
   }
--- src/Modeldata/Data.js
+++ src/Modeldata/Data.js
@@ -1,201 +1,204 @@
-var Data = [
-  {
-    flightName: "IRFLY01",
-    flightNumber: "AniRaj-Fly",
-    flightTime: {
-      depart: "08:30",
-      arrival: "10:30"
+var Data = {
+  flightData: [
+    {
+      flightName: "IRFLY01",
+      flightNumber: "AniRaj-Fly",
+      flightTime: {
+        depart: "08:30",
+        arrival: "10:30"
+      },
+      totalSeats: "10",
+      occupiedSeats: "6",
+      vacateSeats: "4",
+      from: "Delhi",
+      to: "Banglore",
+      pasangers: [
+        {
+          name: "Raj",
+          age: "30",
+          gender: "Male",
+          passport: "CVD2345HT",
+          dob: "13/03/1990",
+          seatNumber: "D1",
+          ancillaryServices: "No",
+          contactNumber: "8978654565",
+          specialService: {
+            wheelchair: "No",
+            food: "Yes"
+          }
+        },
+        {
+          name: "Ani",
+          age: "23",
+          gender: "Male",
+          passport: "FRTE7654MY",
+          dob: "23/06/1996",
+          seatNumber: "D4",
+          ancillaryServices: "No",
+          contactNumber: "7656456588",
+          specialService: {
+            wheelchair: "No",
+            food: "Yes"
+          }
+        },
+        {
+          name: "Allo waliya",
+          age: "67",
+          gender: "Male",
+          passport: "TYU67548THG",
+          dob: "12/02/1962",
+          seatNumber: "D7",
+          ancillaryServices: "Yes",
+          contactNumber: "8798675434",
+          specialService: {
+            wheelchair: "Yes",
+            food: "No"
+          }
+        },
+        {
+          name: "Sheela",
+          age: "23",
+          gender: "Female",
+          passport: "TYHJ7654ML",
+          dob: "22/06/1997",
+          seatNumber: "D5",
+          ancillaryServices: "No",
+          contactNumber: "8767564534",
+          specialService: {
+            wheelchair: "No",
+            food: "No"
+          }
+        },
+        {
+          name: "Rex",
+          age: "59",
+          gender: "Male",
+          passport: "MKYT8976PY",
+          dob: "",
+          seatNumber: "D9",
+          ancillaryServices: "No",
+          contactNumber: "",
+          specialService: {
+            wheelchair: "No",
+            food: "No"
+          }
+        },
+        {
+          name: "Ankita",
+          age: "29",
+          gender: "Female",
+          passport: "",
+          dob: "12/06/1991",
+          seatNumber: "D8",
+          ancillaryServices: "No",
+          contactNumber: "8978676545",
+          specialService: {
+            wheelchair: "no",
+            food: "No"
+          }
+        }
+      ]
     },
-    totalSeats: "10",
-    occupiedSeats: "6",
-    vacateSeats: "4",
-    from: "Delhi",
-    to: "Banglore",
-    pasangers: [
-      {
-        name: "Raj",
-        age: "30",
-        gender: "Male",
-        passport: "CVD2345HT",
-        dob: "13/03/1990",
-        seatNumber: "D1",
-        ancillaryServices: "No",
-        contactNumber: "8978654565",
-        specialService: {
-          wheelchair: "No",
-          food: "Yes"
-        }
+    {
+      flightName: "IRFLY02",
+      flightNumber: "AniRaj-Fly",
+      flightTime: {
+        depart: "13:30",
+        arrival: "15:30"
       },
-      {
-        name: "Ani",
-        age: "23",
-        gender: "Male",
-        passport: "FRTE7654MY",
-        dob: "23/06/1996",
-        seatNumber: "D4",
-        ancillaryServices: "No",
-        contactNumber: "7656456588",
-        specialService: {
-          wheelchair: "No",
-          food: "Yes"
+      totalSeats: "10",
+      occupiedSeats: "3",
+      vacateSeats: "7",
+      from: "Delhi",
+      to: "Mumbai",
+      pasangers: [
+        {
+          name: "Shalu",
+          age: "24",
+          gender: "Female",
+          passport: "",
+          dob: "22/08/1997",
+          seatNumber: "B3",
+          ancillaryServices: "No",
+          contactNumber: "7867656565",
+          specialService: {
+            wheelchair: "No",
+            food: "Yes"
+          }
+        },
+        {
+          name: "Rajeev",
+          age: "59",
+          gender: "Male",
+          passport: "TYUHJ6756OP",
+          dob: "12/08/1960",
+          seatNumber: "B5",
+          ancillaryServices: "Yes",
+          contactNumber: "7878989809",
+          specialService: {
+            wheelchair: "Yes",
+            food: "Yes"
+          }
+        },
+        {
+          name: "Benny",
+          age: "26",
+          gender: "male",
+          passport: "",
+          dob: "12/09/1994",
+          seatNumber: "B8",
+          ancillaryServices: "No",
+          contactNumber: "3454657687",
+          specialService: {
+            wheelchair: "No",
+            food: "No"
+          }
         }
-      },
-      {
-        name: "Allo waliya",
-        age: "67",
-        gender: "Male",
-        passport: "TYU67548THG",
-        dob: "12/02/1962",
-        seatNumber: "D7",
-        ancillaryServices: "Yes",
-        contactNumber: "8798675434",
-        specialService: {
-          wheelchair: "Yes",
-          food: "No"
-        }
-      },
-      {
-        name: "Sheela",
-        age: "23",
-        gender: "Female",
-        passport: "TYHJ7654ML",
-        dob: "22/06/1997",
-        seatNumber: "D5",
-        ancillaryServices: "No",
-        contactNumber: "8767564534",
-        specialService: {
-          wheelchair: "No",
-          food: "No"
-        }
-      },
-      {
-        name: "Rex",
-        age: "59",
-        gender: "Male",
-        passport: "MKYT8976PY",
-        dob: "",
-        seatNumber: "D9",
-        ancillaryServices: "No",
-        contactNumber: "",
-        specialService: {
-          wheelchair: "No",
-          food: "No"
-        }
-      },
-      {
-        name: "Ankita",
-        age: "29",
-        gender: "Female",
-        passport: "",
-        dob: "12/06/1991",
-        seatNumber: "D8",
-        ancillaryServices: "No",
-        contactNumber: "8978676545",
-        specialService: {
-          wheelchair: "no",
-          food: "No"
-        }
-      }
-    ]
-  },
-  {
-    flightName: "IRFLY02",
-    flightNumber: "AniRaj-Fly",
-    flightTime: {
-      depart: "13:30",
-      arrival: "15:30"
+      ]
     },
-    totalSeats: "10",
-    occupiedSeats: "3",
-    vacateSeats: "7",
-    from: "Delhi",
-    to: "Mumbai",
-    pasangers: [
-      {
-        name: "Shalu",
-        age: "24",
-        gender: "Female",
-        passport: "",
-        dob: "22/08/1997",
-        seatNumber: "B3",
-        ancillaryServices: "No",
-        contactNumber: "7867656565",
-        specialService: {
-          wheelchair: "No",
-          food: "Yes"
-        }
+    {
+      flightName: "IRFLY03",
+      flightNumber: "AniRaj-Fly",
+      flightTime: {
+        depart: "14:30",
+        arrival: "15:00"
       },
-      {
-        name: "Rajeev",
-        age: "59",
-        gender: "Male",
-        passport: "TYUHJ6756OP",
-        dob: "12/08/1960",
-        seatNumber: "B5",
-        ancillaryServices: "Yes",
-        contactNumber: "7878989809",
-        specialService: {
-          wheelchair: "Yes",
-          food: "Yes"
+      totalSeats: "10",
+      occupiedSeats: "2",
+      vacateSeats: "8",
+      from: "Delhi",
+      to: "Indore",
+      pasangers: [
+        {
+          name: "Subham",
+          age: "30",
+          gender: "Male",
+          passport: "",
+          dob: "02/12/1990",
+          seatNumber: "C2",
+          ancillaryServices: "No",
+          contactNumber: "8978987867",
+          specialService: {
+            wheelchair: "No",
+            food: "No"
+          }
+        },
+        {
+          name: "Shatto",
+          age: "25",
+          gender: "Female",
+          passport: "HTYU6576TY",
+          dob: "20/09/1994",
+          seatNumber: "C7",
+          ancillaryServices: "No",
+          contactNumber: "",
+          specialService: {
+            wheelchair: "No",
+            food: "No"
+          }
         }
-      },
-      {
-        name: "Benny",
-        age: "26",
-        gender: "male",
-        passport: "",
-        dob: "12/09/1994",
-        seatNumber: "B8",
-        ancillaryServices: "No",
-        contactNumber: "3454657687",
-        specialService: {
-          wheelchair: "No",
-          food: "No"
-        }
-      }
-    ]
-  },
-  {
-    flightName: "IRFLY03",
-    flightNumber: "AniRaj-Fly",
-    flightTime: {
-      depart: "14:30",
-      arrival: "15:00"
-    },
-    totalSeats: "10",
-    occupiedSeats: "2",
-    vacateSeats: "8",
-    from: "Delhi",
-    to: "Indore",
-    pasangers: [
-      {
-        name: "Subham",
-        age: "30",
-        gender: "Male",
-        passport: "",
-        dob: "02/12/1990",
-        seatNumber: "C2",
-        ancillaryServices: "No",
-        contactNumber: "8978987867",
-        specialService: {
-          wheelchair: "No",
-          food: "No"
-        }
-      },
-      {
-        name: "Shatto",
-        age: "25",
-        gender: "Female",
-        passport: "HTYU6576TY",
-        dob: "20/09/1994",
-        seatNumber: "C7",
-        ancillaryServices: "No",
-        contactNumber: "",
-        specialService: {
-          wheelchair: "No",
-          food: "No"
-        }
-      }
-    ]
-  }
-];
+      ]
+    }
+  ]
+};
+export default Data;
