2097f9d289b0829822fe1b77635d7540b5c215b9
ANJANI MISHRA <38608850+aniraj2206@users.noreply.github.com>
2019-12-12 16:23:09 +0530

    test
    test

--- src/Flightlist/Flightlist.js
+++ src/Flightlist/Flightlist.js
@@ -1,9 +1,48 @@
 import React from "react";
+import "./Flightlist.scss";
 
 const FlightList = props => {
   const flightObj = props.flightList;
+
+  const showPessangerList = flightNo => {
+    props.history.push(`/pessenger-list?flightNo=${flightNo}`);
+  };
+
   const elem = flightObj.map(flights => {
-    return <div>{flights.flightName}</div>;
+    return (
+      <div className="flCont">
+        <div className="flName">
+          {flights.flightNumber} ({flights.flightName})
+        </div>
+        <div className="flToFrom">
+          <div className="flFromSec">
+            <span className="fromCaption">From: </span>
+            <span className="flFrom">
+              {flights.from} ({flights.flightTime.depart})
+            </span>
+          </div>
+          <div className="flToSec">
+            <span className="toCaption">To: </span>
+            <span className="flFrom">
+              {flights.to} ({flights.flightTime.arrival})
+            </span>
+          </div>
+        </div>
+        <div className="flPesDet">
+          <span className="flAOccSeats">
+            Seats: {flights.occupiedSeats} ({flights.totalSeats})
+          </span>
+        </div>
+        <div className="flNavToPessList">
+          <button
+            onClick={() => showPessangerList(flights.flightNumber)}
+            className="flNavToPessBtn"
+          >
+            Pessenger's List
+          </button>
+        </div>
+      </div>
+    );
   });
   return <>{elem}</>;
 };
